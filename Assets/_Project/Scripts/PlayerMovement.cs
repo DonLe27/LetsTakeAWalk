ae09320fe2f217557c91a9f0abe6a85fb31d6edd
Don <donduyle7@gmail.com>
2021-04-17 23:59:02 -0700

    add prefabs for each area

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -5,9 +5,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
 
-
+    public Rigidbody rb;
     public float speed = 10.0f;
-
+    public float jumpPower = 1.0f;
+    private Vector3 playerVelocity;
+    private float gravityValue = -9.81f;
 
     private void Start()
     {
@@ -26,5 +28,16 @@ public class PlayerMovement : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.None;
         }
+    }
+    void FixedUpdate()
+    {
+        bool groundedPlayer = transform.position.y == 1;
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.VelocityChange);
+        }
+        rb.AddForce(new Vector3(0, gravityValue, 0), ForceMode.Acceleration);
     }
 }
