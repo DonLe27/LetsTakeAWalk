53bbc7eb4a0b4b1bec052e68205bb5cb7d9afd22
Don <donduyle7@gmail.com>
2021-04-17 21:27:31 -0700

    setting up map layout

---
+++ Assets/_Project/Scripts/ScenePartLoader.cs
@@ -0,0 +1,113 @@
+// Referenced: https://github.com/amel-unity/Multi-Scene-workflow/blob/master/Assets/Creator%20Kit%20-%20FPS/Scripts/System/ScenePartLoader.cs
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum CheckMethod
+{
+    Distance,
+    Trigger
+}
+public class ScenePartLoader : MonoBehaviour
+{
+
+    public Transform player;
+    public CheckMethod checkMethod;
+    public float loadRange;
+
+    //Scene state
+    private bool isLoaded;
+    private bool shouldLoad;
+    void Start()
+    {
+        //verify if the scene is already open to avoid opening a scene twice
+        if (SceneManager.sceneCount > 0)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if (scene.name == gameObject.name)
+                {
+                    isLoaded = true;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Checking which method to use
+        if (checkMethod == CheckMethod.Distance)
+        {
+            DistanceCheck();
+        }
+        else if (checkMethod == CheckMethod.Trigger)
+        {
+            TriggerCheck();
+        }
+    }
+
+    void DistanceCheck()
+    {
+        //Checking if the player is within the range
+        if (Vector3.Distance(player.position, transform.position) < loadRange)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            //Loading the scene, using the gameobject name as it's the same as the name of the scene to load
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            //We set it to true to avoid loading the scene twice
+            isLoaded = true;
+        }
+    }
+
+    void UnLoadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = false;
+        }
+    }
+
+    void TriggerCheck()
+    {
+        //shouldLoad is set from the Trigger methods
+        if (shouldLoad)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+
+
+}
