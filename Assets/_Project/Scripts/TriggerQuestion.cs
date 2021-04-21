b0237748e7f434a7a36f1e5c6afb30583319f2ab
ryand3031 <ryand3031@gmail.com>
2021-04-20 22:36:38 -0700

    created a prefab for the trigger block

--- Assets/_Project/Scripts/TriggerQuestion.cs
+++ Assets/_Project/Scripts/TriggerQuestion.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 public class TriggerQuestion : MonoBehaviour
 {
     public Text displayText;
+    public bool isRetriggerable = false;
+    bool wasTriggered = false;
 
     void OnTriggerEnter(Collider collisionInfo){
-        Debug.Log("collided");
-        if (collisionInfo.tag == "Player")
+        //Debug.Log("collided");
+        if (!wasTriggered) //checks if the block has been triggered
         {
-            displayText.text = "Who are you?";
+            
+            if (collisionInfo.tag == "Player") //if player collided with trigger
+            {
+                //Debug.Log("Triggered");
+                displayText.text = "Who are you?";
+                if(!isRetriggerable) // if block is not retriggerable set wasTriggered to false to prevent retriggering
+                    wasTriggered = true;
+            }
         }
     }
 
