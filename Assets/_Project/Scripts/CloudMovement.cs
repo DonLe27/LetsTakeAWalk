f986329db55c17dc43d8e06d8fbb65fd5b55d62a
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-05-05 01:45:42 -0700

    despawn clouds when past boundary

--- Assets/_Project/Scripts/CloudMovement.cs
+++ Assets/_Project/Scripts/CloudMovement.cs
@@ -7,10 +7,19 @@ public class CloudMovement : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] Vector3 direction = new Vector3(-1, 0, 1);
     [SerializeField] float speed = 3f;
+    [SerializeField] float boundaryX = -10;
+    [SerializeField] float boundaryZ = 2400;
+    void Start()
+    {
 
+    }
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < boundaryX || transform.position.z > boundaryZ)
+        {
+            Destroy(gameObject);
+        }
         transform.position += speed * Time.deltaTime * direction.normalized;
     }
 }
--- Assets/_Project/Scripts/CloudSpawner.cs
+++ Assets/_Project/Scripts/CloudSpawner.cs
@@ -9,8 +9,8 @@ public class CloudSpawner : MonoBehaviour
     public GameObject cloud2;
     public GameObject cloud3;
     public GameObject cloud4;
-    private List<GameObject> clouds;
-    public float rate = 10;
+    private List<GameObject> clouds = new List<GameObject>();
+    [SerializeField] private float cloudPerSec = 1;
     private Vector3 max;
     private Vector3 min;
 
@@ -22,19 +22,18 @@ public class CloudSpawner : MonoBehaviour
         clouds.Add(cloud2);
         clouds.Add(cloud3);
         clouds.Add(cloud4);
-        Debug.Log(clouds);
+        InvokeRepeating("SpawnCloud", 1.0f, 1.0f / cloudPerSec);
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnCloud()
     {
-        if (Time.time % Mathf.Round(100 / rate) == 0f)
-        {
-            Vector3 location = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
-            Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
-            GameObject temp = clouds[Random.Range(0, 4)];
-            Debug.Log(temp);
-            // newCloud = Instantiate(temp, location, rotation);
-        }
+
+        Debug.Log("spawned cloud");
+        Vector3 location = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+        Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        GameObject temp = clouds[Random.Range(0, 4)];
+        Instantiate(temp, location, rotation);
+
     }
 }
