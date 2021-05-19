0e11dc4936f2eddee9a343e021e317bee66519d4
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-05-19 02:41:44 -0700

    add spirit question ui

---
+++ Assets/_Project/UI/SpiritQuestionDisplay.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritQuestionDisplay : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI text;
+}
