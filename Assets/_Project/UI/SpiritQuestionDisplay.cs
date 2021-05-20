5894de778d440dc5206f763ff1f532b4d3ebe29a
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-05-20 00:44:17 -0700

    add fading spirit and questions

--- Assets/_Project/UI/SpiritQuestionDisplay.cs
+++ Assets/_Project/UI/SpiritQuestionDisplay.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class SpiritQuestionDisplay : MonoBehaviour
 {
     [SerializeField] private GameObject questionComponent;
+    [SerializeField] private CanvasGroup canvasGroup;
     //[SerializeField] private Vector2 dimensionOffset = new Vector2(1000, 600);
     public TMPro.TextMeshProUGUI textMesh;
     public RectTransform imageRectTransform;
+    private Renderer spiritRenderer;
+    public bool isFading = false;
+    [SerializeField]
+    private SpiritSpawner spiritSpawner;
 
 
     public void SetImageRectTransform(Vector2 dimensions)
@@ -15,9 +20,39 @@ public class SpiritQuestionDisplay : MonoBehaviour
         imageRectTransform.sizeDelta = dimensions; //+ dimensionOffset;
         //Debug.Log(imageRectTransform.sizeDelta);
     }
-    public void SetQuestionComponentActive(bool val)
+    public void SetQuestionComponentActive(float duration)
     {
-        questionComponent.SetActive(val);
+        isFading = true;
+        StartCoroutine(DoFade(canvasGroup.alpha, 1, duration));
+    }
+
+    public void SetQuestionComponentInactive(float duration, GameObject spirit)
+    {
+        spiritRenderer = spirit.GetComponent<Renderer>();
+        StartCoroutine(DoFade(canvasGroup.alpha, 0, duration, spirit));
+    }
+
+    public IEnumerator DoFade(float start, float end, float duration, GameObject spirit = null)
+    {
+        float counter = 0f;
+        while (counter < duration)
+        {
+            if (spiritRenderer != null && end == 0)
+            {
+                Color temp = spiritRenderer.material.color;
+                temp.a = Mathf.Lerp(start, end, counter / duration);
+                spiritRenderer.material.color = temp;
+            }
+            counter += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+            yield return null;
+        }
+        if (end == 0)
+        {
+            isFading = false;
+            spiritRenderer = null;
+            spiritSpawner.DespawnSpirit(spirit);
+        }
     }
 
 }
