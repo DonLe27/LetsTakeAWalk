ac716ea396539fe4f9a0207021956b6e3452fe34
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-05-20 20:26:52 -0700

    change UI to screen overlay

--- Assets/_Project/Scripts/PlayerScripts/ManagePlayerData.cs
+++ Assets/_Project/Scripts/PlayerScripts/ManagePlayerData.cs
@@ -32,17 +32,21 @@ public class ManagePlayerData : MonoBehaviour
     //isAdding is a bool to tell if the item is being added to players inventory
     //if !isAdded item is being removed from players inventory
 
-    public void updateIngredients(IngredientID id, bool isAdding){
-        if(isAdding){
-            playerData.ingredients[(int)id]+=1;
+    public void updateIngredients(IngredientID id, bool isAdding)
+    {
+        if (isAdding)
+        {
+            playerData.ingredients[(int)id] += 1;
         }
-        else{
-            playerData.ingredients[(int)id]-=1;
+        else
+        {
+            playerData.ingredients[(int)id] -= 1;
         }
     }
 
     //returns how many units of a given item the player has in their inventory based on the ingredient ID
-    public int getIngredientCount(int index){
+    public int getIngredientCount(int index)
+    {
         return playerData.ingredients[index];
 
     }
