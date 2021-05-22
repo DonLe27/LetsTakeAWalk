4890f3a69d78bfa133e3318ee03427f470ea8c5b
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-05-22 03:35:16 -0700

    change lighting for night

--- Assets/ParticleCollision.cs
+++ Assets/ParticleCollision.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void OnParticleCollision(GameObject particle)
+    void OnParticleTrigger()
     {
-        Debug.Log("collided");
-        particle.GetComponent<ParticleSystemRenderer>().material.color = Color.green;
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+        // particles
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+        // get
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        // iterate
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(255, 0, 0, 255);
+            enter[i] = p;
+        }
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.startColor = new Color32(0, 255, 0, 255);
+            exit[i] = p;
+        }
+
+        // set
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
     }
 
 }
