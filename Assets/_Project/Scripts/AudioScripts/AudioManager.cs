1e62a3abe0d9e3d3f08ec9a5f75d1a15446085fc
Justin Hee <justinhee118@gmail.com>
2021-05-22 19:50:18 -0700

    implemented fade in audio

--- Assets/_Project/Scripts/AudioScripts/AudioManager.cs
+++ Assets/_Project/Scripts/AudioScripts/AudioManager.cs
@@ -2,6 +2,11 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
 
 public class AudioManager : MonoBehaviour
 {
@@ -12,7 +17,7 @@ public class AudioManager : MonoBehaviour
     public static AudioManager instance;
 
     void Awake() {
-
+        //check if an AudioManager is in the current scene, if so then destroy the current one
         if(instance == null)
             instance = this;
         else
@@ -20,6 +25,7 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+
         DontDestroyOnLoad(gameObject);
 
         foreach (Sound s in sounds)
@@ -30,11 +36,14 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+        //song that will play first
         defaultSound = "arpeggios";
     }
     void Start()
     {
-        SwapTrack(defaultSound);
+        currentSound = defaultSound;
+        Play(defaultSound);
+        SwapTrack("rests");
     }
 
     // Update is called once per frame
@@ -42,6 +51,7 @@ public class AudioManager : MonoBehaviour
     {
         
     }
+    //play song given name
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if(s == null)
@@ -49,22 +59,66 @@ public class AudioManager : MonoBehaviour
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
+        currentSound = name;
         Debug.Log("Sound: " + name + " playing");
         s.source.Play();
     }
 
-    public void SwapTrack(string newClip)
+    //switch the current song to something else; fade out the old one
+    public void SwapTrack(string newSound)
     {
-        if(currentSound != newClip)
+        if(currentSound != newSound)
         {
-            currentSound = newClip;
-            Play(newClip);
+            StopAllCoroutines();
+            StartCoroutine(FadeToTrack(newSound));
         }
     }
 
+    //play default song
     public void ReturnToDefault()
     {
         SwapTrack(defaultSound);
+    }
+
+    private IEnumerator FadeToTrack(string newSong) 
+    {
+        Sound oldSong = Array.Find(sounds, sound => sound.name == currentSound);
+        if(oldSong == null)
+        {
+            Debug.LogWarning("Sound: " + currentSound + " not found!");
+            yield return null;
+        }
+        Debug.Log("Sound: " + currentSound + " fading");
+
+        float timeToFade = 2f;
+        float timeElapsed = 0;
+        
+        //fade old song out
+        while(timeElapsed < timeToFade)
+        {
+            oldSong.source.volume = Mathf.Lerp(.1f, 0, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        oldSong.source.Stop();
+        //reset oldSong's volume for the next time it plays
+        oldSong.source.volume = .1f;
+
+        //wait for 3 seconds before playing the next song
+        yield return new WaitForSeconds(3);
+        Play(newSong);
+
+        Sound song = Array.Find(sounds, sound => sound.name == newSong);
+        timeElapsed = 0;
+        while(timeElapsed < timeToFade)
+        {
+            song.source.volume = Mathf.Lerp(0, .1f, timeElapsed/timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+
+
     }
 
 
