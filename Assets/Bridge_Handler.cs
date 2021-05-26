3d42311defe79ea6c4cbacbc74a5417dae4166fb
Marii Yoshida <70412678+mariiyoshida@users.noreply.github.com>
2021-05-25 23:28:44 -0700

    [my] bridge body water

---
+++ Assets/Bridge_Handler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge_Handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Pumpkin Spice Latte");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Bridge Moves");
+
+
+    }
+
+
+
+}
